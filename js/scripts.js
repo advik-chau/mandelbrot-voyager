4ee6a64e2ddb128698dca38057688de1ae036fc6
Advik Chaudhary <advikchau@gmail.com>
2025-06-29 13:35:12 +0800

    bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
+function getSettingData() {
+      document.getElementById("itVal").innerHTML = document.getElementById("it").value;
+      document.getElementById("it2Val").innerHTML = document.getElementById("it2").value;
+      main();
+}
+
 function main() {
     var canvas_element = document.getElementById("mainCanvas");
 
     canvas_element.width = window.innerWidth;
     canvas_element.height = window.innerHeight;
+
+    const multiplier = document.getElementById("it").value;
+    const startZ = document.getElementById("it2").value;
+    console.log("MULTIPLIER", multiplier)
 
     var gl = canvas_element.getContext("webgl");
 
@@ -13,6 +23,7 @@ function main() {
       gl_Position = vec4(a_Position.x, a_Position.y, 0.0, 1.0);
     }`;
 
+
     var fragmentShaderCode = `
     precision highp float;
     uniform vec2 u_zoomCenter;
@@ -20,7 +31,7 @@ function main() {
     uniform int u_maxIterations;
 
     vec2 nextTerm(vec2 z, vec2 c) {
-      return mat2(z,-z.y,z.x)*z*1.3 + c;
+      return mat2(z,-z.y,z.x)*z*${multiplier} + c;
     }
     vec3 hsvTOrgb(vec3 c) {
         vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
@@ -30,7 +41,7 @@ function main() {
     void main() {
       vec2 uv = gl_FragCoord.xy / vec2(800.0, 800.0);
       vec2 c = u_zoomCenter + (uv * 4.0 - vec2(5.0, 2.3)) * (u_zoomSize / 4.0);
-      vec2 z = vec2(0.0);   
+      vec2 z = vec2(${startZ});   
       bool outsideSet = false;
       int iterations = 0;
 
@@ -133,4 +144,6 @@ function main() {
 
     renderFrame();
 }
+
+getSettingData();
 main();
